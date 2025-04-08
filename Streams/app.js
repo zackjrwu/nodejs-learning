ade050670c066620b24cbe4b293fee820482f4e1
ZackMini <wuzhongren@ZackMini.lan>
2025-04-09 01:17:31 +0800

    feat: Add createWriteStream

--- Streams/app.js
+++ Streams/app.js
@@ -7,11 +7,18 @@ let totalChunkSize = 0;
 let currentChunkSize = 0;
 
 // 建立可讀串流
+// 可讀串流的預設 chunk 大小為 64KB = 1024 * 64 = 65536
 const readable = fs.createReadStream('./story.txt', {
   encoding: 'utf8',
   // highWaterMark: 1 * 1024 // 可自訂 chunk 大小
 });
 
+// 建立可寫串流
+// 可寫串流的預設 chunk 大小為 16KB = 1024 * 16 = 16384
+const writable = fs.createWriteStream('./story-copy.txt', {
+  // encoding: 'utf8',
+  // highWaterMark: 1 * 1024 // 可自訂 chunk 大小
+});
 console.log('Default chunk size:', defaultChunkSize);
 
 // 每次讀到資料時的 callback
@@ -19,7 +26,7 @@ const onChunk = (chunk) => {
   chunkCount++;
   currentChunkSize = Buffer.byteLength(chunk);
   totalChunkSize += currentChunkSize;
-
+  writable.write(chunk);
   console.log('Current chunk size:', currentChunkSize);
   console.log('Total chunk size  :', totalChunkSize);
   console.log('----------------------------');
